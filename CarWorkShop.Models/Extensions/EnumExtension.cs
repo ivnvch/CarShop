ae805d24e08e0a75499ca907791e02af701bd561
ivnvch <tarasovets.dmtiv@gmail.com>
2023-04-18 21:20:04 +0300

    feat: add Extentions folder for display Role

---
+++ CarWorkShop.Models/Extensions/EnumExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+
+namespace CarWorkShop.Models.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this System.Enum enumValue)
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                ?.GetName() ?? "Неопределенный";
+        }
+    }
+}
---
+++ CarWorkShop.Service/Interfaces/IProfileService.cs
@@ -0,0 +1,13 @@
+﻿using CarWorkShop.Models.Entity;
+using CarWorkShop.Models.Response;
+using CarWorkShop.Models.ViewModel.Profile;
+
+namespace CarWorkShop.Service.Interfaces
+{
+    public interface IProfileService
+    {
+        Task<BaseResponse<ProfileViewModel>> GetProfile(string firstName);
+
+        Task<BaseResponse<Profile>> Save(ProfileViewModel viewModel);
+    }
+}
