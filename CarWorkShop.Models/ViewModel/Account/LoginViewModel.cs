78a5ab3f115a45ef5dca0e0cd4ce8cd19d761cf0
ivnvch <tarasovets.dmtiv@gmail.com>
2023-04-20 23:56:00 +0300

    feat: add LoginView and RegisterView

--- CarWorkShop.Models/ViewModel/Account/LoginViewModel.cs
+++ CarWorkShop.Models/ViewModel/Account/LoginViewModel.cs
@@ -5,7 +5,7 @@ namespace CarWorkShop.Models.ViewModel.Account
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Введите имя")]
-        [MaxLength(20, ErrorMessage = "Логин должен иметь длину не более 20 символов")]
+        [MaxLength(30, ErrorMessage = "Логин должен иметь длину не более 30 символов")]
         [MinLength(5, ErrorMessage = "Логин должен иметь длину более 5 символов")]
         public string Login { get; set; }
 
