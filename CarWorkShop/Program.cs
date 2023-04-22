5b6ea0c7d2acb23704d3d01e28729d7d0b4a2c30
ivnvch <tarasovets.dmtiv@gmail.com>
2023-04-22 07:25:40 +0300

    refactor: add navbar and a little modification

--- CarWorkShop/Program.cs
+++ CarWorkShop/Program.cs
@@ -64,6 +64,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Record}/{action=CreateRecord}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
