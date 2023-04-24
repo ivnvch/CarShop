d832e4d4940efd0b19a94a213782b3efb274548b
ivnvch <tarasovets.dmtiv@gmail.com>
2023-04-24 23:58:31 +0300

    refactor: add DetailView

---
+++ CarWorkShop.DAL/Migrations/20230423223147_ChangeProfileTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarWorkShop.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeProfileTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileId",
+                table: "Owners");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileId",
+                table: "Owners",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
---
+++ CarWorkShop/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using CarWorkShop.Models.ViewModel.Profile;
+using CarWorkShop.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWorkShop.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+        private readonly IProfileService _profileService;
+
+        public ProfileController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Detail()
+        {
+            var login = User.Identity.Name;
+            var  response = await _profileService.GetProfile(login);
+            if (response.StatusCode == Models.Enum.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Save(ProfileViewModel viewModel)
+        {
+            ModelState.Remove("Login");
+            if (ModelState.IsValid)
+            {
+                 await _profileService.Save(viewModel);
+            }
+            return RedirectToAction("Detail");
+        }
+    }
+}
